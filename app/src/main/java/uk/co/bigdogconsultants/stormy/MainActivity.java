05571b70494aa2cd137c4a3fb75f328b5daa095a
Steve Hunter <steve.hunter@bigdogconsultants.co.uk>
2019-09-30 20:39:35 +0100

    linking model to view via data binding

--- app/src/main/java/uk/co/bigdogconsultants/stormy/MainActivity.java
+++ app/src/main/java/uk/co/bigdogconsultants/stormy/MainActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
 import android.util.Log;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
